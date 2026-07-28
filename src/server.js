67388363d7f69f4789569f3e627e30d0ab5d9d35
kuldeep <kuldeeplodi919@gmail.com>
2026-07-28 23:43:12 +0530

    config mongodb

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
+
+require("dotenv").config();
 const app = require("./app");
+
+const connectDB = require("./config/db");
+
+
 
 const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
+    connectDB();
 });
+
+
